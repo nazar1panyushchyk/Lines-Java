7c6d98ca80e3bacff98aae1b97c3f30e609b05b2
Nazar_Panyushchyk <npanusik@gmail.com>
2024-02-19 22:54:04 +0200

    add

---
+++ script.js
@@ -0,0 +1,16 @@
+const result = 5+5+`5`;
+console.log(typeof result);
+
+const email = "nnn@gmail.com"
+console.log(email.includes('@'));
+console.log(email.length);
+
+const str1 = 'My';
+const name1 = 'name';
+const str2 = 'is';
+const fullName = `${str1} ${name1} ${str2} Viktor.`;
+console.log(fullName);
+
+const userName = "Олександро!";
+const payment = "300";
+alert(`Дякуємо, ${userName} До сплати ${payment} гривень.`);
